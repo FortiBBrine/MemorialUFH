09f2d67415597b6061f816cd2f39cebba6429f2c
Aleksandr Steblevets <fortibrinelike@gmail.com>
2023-11-05 21:06:05 +0200

    Почав писати

---
+++ src/main/java/me/sashasteblevets/memorialufh/MemorialUfhApplication.java
@@ -0,0 +1,13 @@
+package me.sashasteblevets.memorialufh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorialUfhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorialUfhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/sashasteblevets/memorialufh/MemorialUfhApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sashasteblevets.memorialufh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorialUfhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
